d8e8200175e97702f61ab0a68094a3b1f431ced4
Pwahaha <34077604+pwahaha@users.noreply.github.com>
2017-11-29 07:07:30 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,16 @@
-// Load discord.js
-var Discord = require("discord.js");
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-// Create a client
-var bot = new Discord.Client();
-
-bot.on("ready", function () {
-	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-bot.on("disconnected", function () {
-	// alert the console
-	console.log("Disconnected.");
-
-	// exit node.js with an error
-	process.exit(1);
+client.on('message', message => {
+    if (message.content === 'heya') {
+    	message.reply("Um...I'm not quite a person... I'm a simply an artificial intelligence program. It would make me happy if you called me Alter Ego. That is the name my master gave me when he created me.
+		      ");
+  	}
 });
 
-// This code will run once the bot receives any message.
-bot.on("message", function (msg) {
-	if (msg.content.indexOf("heya") === 0) {
-		bot.sendMessage(msg.channel, 
-				"Um...I'm not quite a person... I'm a simply an artificial intelligence program. It would make me happy if you called me Alter Ego. That is the name my master gave me when he created me."
-			       );
-
-		// alert the console
-		console.log("pong-ed " + msg.author.username);
-	}
-});
-
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
